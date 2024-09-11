d59b12d242f78c3e118bfa88ce20cd0803c73796
Fergod <fernandodat600@gmail.com>
2024-09-11 18:24:34 -0300

    PRIMEIRA PARTE

---
+++ index.js
@@ -0,0 +1,55 @@
+const { select, input } = require("@inquirer/prompts");
+
+let meta = {
+  value: "Tomar 3L de água por dia",
+  checked: false,
+};
+let metas = [meta];
+
+const cadastrarMeta = async () => {
+  console.log("chegou");
+  const meta = await input({ message: "Digite a meta " });
+
+  if (meta.length == 0) {
+    console.log("A meta não pode ser vazia");
+    return;
+  }
+  metas.push({ value: meta, checked: false });
+};
+
+const start = async () => {
+  while (true) {
+    const opcao = await select({
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar meta",
+          value: "cadastrar",
+        },
+        {
+          name: "Listas metas",
+          value: "listar",
+        },
+        {
+          name: "Sair",
+          value: "sair",
+        },
+      ],
+    });
+    console.log({ opcao, metas });
+    switch (opcao) {
+      case "cadastrar":
+        await cadastrarMeta();
+        console.log(metas);
+        break;
+      case "sistar":
+        console.log("vamos listar as metas");
+        break;
+      case "sair":
+        console.log("Até a próxima");
+        return;
+    }
+  }
+};
+
+start();
